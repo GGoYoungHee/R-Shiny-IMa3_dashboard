6cedeaa0ef9f79b6cf19d73a3f6f562c8a88242a
Sty1ish <89575978+Sty1ish@users.noreply.github.com>
2022-03-07 00:13:28 +0900

    Revert "Merge pull request #13 from GGoYoungHee/ShinyThemes_ERROR_CAUSED"
    This reverts commit 27a66dad91667fff79f894644e99daef2259775a, reversing
    changes made to bd839db92ede9910f06f2d42ca4689fa17fb7dd8.

--- func.R
+++ func.R
@@ -13,24 +13,3 @@ tab_im <- function(text, cor,id){
 
 ## origin version ##
 cores <- c("#0A043C", "#098ebb", "#fdc23a", "#e96449", "#D3BBAF", "#ddcdbf", "#e6c26c") # color range
-
-# 경로설정.
-volumes = c('wd'='.')
-
-# 에러 대처용. shinythemes() 함수 동작하지 않음.
-shinytheme <- function(theme = NULL) {
-  # Check that theme exists
-  if (is.null(theme) || !theme %in% allThemes()) {
-    stop(theme, " is not an available theme. Valid themes are: ",
-         paste(allThemes(), collapse = ", "), ".")
-  }
-  
-  paste0("shinythemes/css/", theme, ".min.css")
-}
-
-
-# 해당 함수가 변경되었음.
-allThemes <- function() {
-  themes <- dir(system.file("shinythemes/css", package = "shinythemes"), ".min.css")
-  sub(".min.css", "", themes)
-}
